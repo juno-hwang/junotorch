eaa8c2763746cd3c8b6befd0d91f98d3ab1a1609
Juno Hwang <52326857+juno-hwang@users.noreply.github.com>
2022-02-15 10:38:53 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="junotools", # Replace with your own username
+    version="0.3.0",
+    author="Juno Hwang",
+    author_email="wnsdh10@snu.ac.kr",
+    description="Custom pytorch modules that I use often",
+    url="https://github.com/juno-hwang/junotorch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy','matplotlib','torch'],
+    python_requires='>=3.6',
+)
