9da5d74ef0c34c453567492dc9b5a1e9b3ff0331
Juno Hwang <52326857+juno-hwang@users.noreply.github.com>
2023-07-20 16:14:02 +0900

    Update biotest.py

--- biotest.py
+++ biotest.py
@@ -2,16 +2,14 @@ from Bio import Entrez
 from Bio import SeqIO
 
 def efetch(db, rettype, id):
-    import io, urllib
+    import io, urllib, ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
     url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={}&rettype={}&id={}&tool=biopython".format(db, rettype, id)
-    with urllib.request.urlopen(url) as handle:
-        with io.TextIOWrapper(handle, encoding='utf-8') as h:
-            return h
+    handle =  urllib.request.urlopen(url)
+    handle =  io.TextIOWrapper(handle, encoding='utf-8')
+    return handle
 
-efetch(db="nucleotide", rettype="fasta", id="MN996528.1")
-
-
-# handle=Entrez.efetch(db="nucleotide", rettype="fasta",id="MN996528.1")
+handle = efetch(db="nucleotide", rettype="fasta", id="MN996528.1")
 for seq_rec1 in SeqIO.parse(handle,"fasta"):
     count=SeqIO.write(seq_rec1,"sars-cov.fasta","fasta")
 
