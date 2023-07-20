7cd7ba0cf87fad2f98664a886ea4783935c5f7e0
Juno Hwang <52326857+juno-hwang@users.noreply.github.com>
2023-07-20 16:15:21 +0900

    Update biotest.py

--- biotest.py
+++ biotest.py
@@ -1,15 +1,47 @@
-from Bio import Entrez
+!pip install Biopython
+
+import Bio
+
+from Bio.Seq import Seq
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+seq1=Seq("ATATATACCCCGGGGGGG")
+
+type(seq1)
+
+for index, letter in enumerate(seq1):
+    print(index,letter)
+
+print(seq1[4])
+
+seq1[0:15]
+
+seq1[0::1]
+
+seq1[::-1]
+
+countG=seq1.count("G")
+countC=seq1.count("C")
+countGC=(countG+countC)/len(seq1)*100
+print(countGC)
+
+
+from Bio.SeqUtils import GC
+GC(seq1)
+
+seq1.complement()
+
+seq1.reverse_complement()
+
 from Bio import SeqIO
+from Bio import Entrez
 
-def efetch(db, rettype, id):
-    import io, urllib, ssl
-    ssl._create_default_https_context = ssl._create_unverified_context
-    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={}&rettype={}&id={}&tool=biopython".format(db, rettype, id)
-    handle =  urllib.request.urlopen(url)
-    handle =  io.TextIOWrapper(handle, encoding='utf-8')
-    return handle
+Entrez.email="자신의 이메일"
 
-handle = efetch(db="nucleotide", rettype="fasta", id="MN996528.1")
+
+handle=Entrez.efetch(db="nucleotide", rettype="fasta",id="MN996528.1")
 for seq_rec1 in SeqIO.parse(handle,"fasta"):
     count=SeqIO.write(seq_rec1,"sars-cov.fasta","fasta")
 
@@ -22,3 +54,21 @@ seq2.complement()
 
 seqrev2=seq2.reverse_complement()
 print(seqrev2)
+
+
+from Bio.Seq import Seq
+
+# DNA 시퀀스
+
+
+start_codon = "ATG"
+
+stop_codons = ["TAA", "TAG", "TGA"]
+
+start_position = seq2.find(start_codon)
+
+stop_position = min([seq2.find(stop_codon, start_position) for stop_codon in stop_codons if seq2.find(stop_codon, start_position) != -1])
+
+extracted_sequence = seq2[start_position:stop_position+3]
+
+print("Extracted Sequence:", extracted_sequence)
